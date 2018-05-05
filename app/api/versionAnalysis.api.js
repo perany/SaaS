18c926f134d9f08de93caa5fe765f59084a7efb3
perany <perany@163.com>
2018-05-05 18:50:55 +0800

    功能分析 接口&数据

--- app/api/versionAnalysis.api.js
+++ app/api/versionAnalysis.api.js
@@ -3,8 +3,8 @@ var api = {
     versionList: function(value) {
         var deferred = $.Deferred();
         $.ajax({
-            url: api.app.domain + 'versionAnalysis/versionList',
-            type: "post",
+            url: api.app.localDomain + 'versionAnalysis/versionList.json',
+            type: "get",
             dataType: "json",
             data: api.app.format(value),
             success: function(response) {
@@ -15,70 +15,114 @@ var api = {
     },
     addUserAnalysis: function(value) {
         var deferred = $.Deferred()
+        var path;
+        switch (value.versionIds){
+            case "01,02,03,04":
+                path=api.app.localDomain+'versionAnalysis/addUserAnalysis1.json';
+                break;
+            default:
+                path=api.app.localDomain+'versionAnalysis/addUserAnalysis2.json';
+                break;
+        }
         $.ajax({
-            url: api.app.domain + 'versionAnalysis/addUserAnalysis',
-            type: "POST",
-            dataType: "json",
-            data: value,
-            success: function(response) {
-                deferred.resolve(response);
-            },
-            error: function(response) {
-                alert('error');
-                deferred.resolve(response);
-
+            // url: api.app.domain + 'activesAnalysis/cpgaAnalysis',
+            url:path,
+            type: "post",
+            dataType: 'json',
+            data: api.app.format(value),
+            success: function (response) {
+                api.app.goBack(response, deferred)
             }
         });
         return deferred
     },
     activeUserAnalysis: function(value) {
         var deferred = $.Deferred()
+        var path;
+        switch (value.versionIds){
+            case "01,02,03,04":
+                path=api.app.localDomain+'versionAnalysis/activeUserAnalysis1.json';
+                break;
+            default:
+                path=api.app.localDomain+'versionAnalysis/activeUserAnalysis2.json';
+                break;
+        }
         $.ajax({
-            url: api.app.domain + 'versionAnalysis/activeUserAnalysis',
+            url:path,
             type: "post",
             dataType: "json",
             data: api.app.format(value),
-            success: function(response) {
-                deferred.resolve(response);
+            success: function (response) {
+                api.app.goBack(response, deferred)
             }
         });
         return deferred
     },
     registerUserAnalysis: function(value) {
         var deferred = $.Deferred()
+        var path;
+        switch (value.versionIds){
+            case "01,02,03,04":
+                path=api.app.localDomain+'versionAnalysis/registerUserAnalysis1.json';
+                break;
+            default:
+                path=api.app.localDomain+'versionAnalysis/registerUserAnalysis2.json';
+                break;
+        }
         $.ajax({
-            url: api.app.domain + 'versionAnalysis/registerUserAnalysis',
+            // url: api.app.domain +'activesAnalysis/paidUserCostAnalysis',
+            url:path,
             type: "post",
             dataType: "json",
             data: api.app.format(value),
-            success: function(response) {
-                deferred.resolve(response);
+            success: function (response) {
+                api.app.goBack(response, deferred)
             }
         });
         return deferred
     },
     padUserAnalysis: function(value) {
         var deferred = $.Deferred()
+        var path;
+        switch (value.versionIds){
+            case "01,02,03,04":
+                path=api.app.localDomain+'versionAnalysis/paidUserCostAnalysis1.json';
+                break;
+            default:
+                path=api.app.localDomain+'versionAnalysis/paidUserCostAnalysis2.json';
+                break;
+        }
         $.ajax({
-            url: api.app.domain + 'versionAnalysis/padUserAnalysis',
+            // url: api.app.domain +'activesAnalysis/paidUserCostAnalysis',
+            url:path,
             type: "post",
             dataType: "json",
             data: api.app.format(value),
-            success: function(response) {
-                deferred.resolve(response);
+            success: function (response) {
+                api.app.goBack(response, deferred)
             }
         });
         return deferred
     },
     remainUserAnalysis: function(value) {
         var deferred = $.Deferred()
+        var path;
+        switch (value.versionIds){
+            case "01,02,03,04":
+                path=api.app.localDomain+'versionAnalysis/retainedUserCostAnalysis1.json';
+                break;
+            default:
+                path=api.app.localDomain+'versionAnalysis/retainedUserCostAnalysis2.json';
+                break;
+        }
         $.ajax({
-            url: api.app.domain + 'versionAnalysis/remainUserAnalysis',
+            // url: api.app.domain + 'activesAnalysis/retainedUserCostAnalysis',
+            url:path,
             type: "post",
             dataType: "json",
             data: api.app.format(value),
-            success: function(response) {
-                deferred.resolve(response);
+            success: function (response) {
+                api.app.goBack(response, deferred)
             }
         });
         return deferred
